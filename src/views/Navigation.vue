353852a923447b45b499738293409ae88b42d8cc
André Masson <masson.andre@gmail.com>
2018-06-28 17:46:38 -0300

    Link para redes sociais no topo

--- src/views/Navigation.vue
+++ src/views/Navigation.vue
@@ -1,8 +1,24 @@
 <template>
   <nav class="navbar navbar-light bg-light static-top">
     <div class="container">
-      <a class="navbar-brand" href="#">Start Bootstrap</a>
-      <a class="btn btn-primary" href="#">Sign In</a>
+      <a class="navbar-brand" href="#">André Masson</a>
+      <ul class="list-inline mb-0">
+        <li class="list-inline-item mr-3">
+          <a href="https://twitter.com/ar_masson">
+            <i class="icon-social-twitter m-auto text-primary"></i>
+          </a>
+        </li>
+        <li class="list-inline-item mr-3">
+          <a href="https://web.facebook.com/andre.masson.7">
+            <i class="icon-social-facebook m-auto text-primary"></i>
+          </a>
+        </li>
+        <li class="list-inline-item">
+          <a href="https://www.instagram.com/masson_andre/">
+            <i class="icon-social-instagram m-auto text-primary"></i>
+          </a>
+        </li>
+      </ul>
     </div>
   </nav>
 </template>
